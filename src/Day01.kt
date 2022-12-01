4608febbb907bc19f5698d34d28c0ae376f76832
Xavier Monset <xavier.monset@gmail.com>
2022-12-01 14:56:44 +0100

    Result

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,45 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max_somme = 0
+        var somme = 0
+        input.forEach {
+
+            if (it != "") {
+                somme += it.toInt()
+            } else {
+                max_somme = if (somme > max_somme) somme else max_somme
+                somme = 0
+            }
+        }
+
+        return max_somme
+
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val tableau = mutableListOf<Int>()
+        var somme = 0
+        input.forEach {
+
+            if (it != "") {
+                somme += it.toInt()
+            } else {
+                tableau.add(somme)
+                somme = 0
+            }
+        }
+        tableau.add(somme)
+        tableau.sortDescending()
+        return tableau.subList(0, 3).fold(0) { somme, element -> somme + element }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+
 }
