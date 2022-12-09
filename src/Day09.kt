9e40f2d905751b2e0df6d5a08d6fa27fd4553729
Xavier Monset <xavier.monset@gmail.com>
2022-12-09 16:16:38 +0100

    Day 9

--- src/Day09.kt
+++ src/Day09.kt
@@ -7,56 +7,14 @@ fun main() {
         return sqrt((x_H - x_T).toFloat().pow(2) + (y_H - y_T).toFloat().pow(2))
     }
 
-    fun part1(input: List<String>): Int {
-        val liste = mutableSetOf<position_occupee>()
-
-        var posx_H = 0
-        var posy_H = 0
-        var posx_T = 0
-        var posy_T = 0
-        liste.add(position_occupee(posx_T, posy_T))
-
-        input.forEach { ligne ->
-            val direction = ligne.substring(0, 1)
-            val combien = ligne.substring(2).toInt()
-
-            for (i in 1..combien) {
-                when (direction) {
-                    "R" -> posx_H++
-                    "L" -> posx_H--
-                    "U" -> posy_H++
-                    "D" -> posy_H--
-                }
-                val distance = euclidienne(posx_H, posy_H, posx_T, posy_T)
-
-                if (distance > 2) {
-                    // diagonale
-                    posy_T += if (posy_H > posy_T) 1 else -1
-                    posx_T += if (posx_H > posx_T) 1 else -1
-
-                } else if (abs(posx_H - posx_T) > 1) {
-                    // Sur le meme Y.
-                    posx_T += if (posx_H > posx_T) 1 else -1
-
-                } else if (abs((posy_T - posy_H)) > 1) {
-                    // Sur le meme X
-                    posy_T += if (posy_H > posy_T) 1 else -1
-                }
-                liste.add(position_occupee(posx_T, posy_T))
-            }
-        }
-        return liste.size
-
-    }
-
-    fun part2(input: List<String>): Int {
+    fun solution(input: List<String>, nombreDeNoeuds: Int): Int {
 
         val liste = mutableSetOf<position_occupee>()
 
-        var posx = arrayOf(0,0,0,0,0,0,0,0,0,0)
-        var posy = arrayOf(0,0,0,0,0,0,0,0,0,0)
+        var posx = Array(nombreDeNoeuds) { 0 }
+        var posy = Array(nombreDeNoeuds) { 0 }
 
-        liste.add(position_occupee(posx[9], posy[9]))
+        liste.add(position_occupee(posx[nombreDeNoeuds - 1], posy[nombreDeNoeuds - 1]))
 
         input.forEach { ligne ->
             val direction = ligne.substring(0, 1)
@@ -69,26 +27,26 @@ fun main() {
                     "U" -> posy[0]++
                     "D" -> posy[0]--
                 }
-                for (j in 1..9) {
-                    val distance = euclidienne(posx[j -1], posy[j -1], posx[j ], posy[j ])
+                for (j in 1 until nombreDeNoeuds) {
+                    val distance = euclidienne(posx[j - 1], posy[j - 1], posx[j], posy[j])
 
                     if (distance > 2) {
                         // diagonale
-                        posy[j ] += if (posy[j -1] > posy[j ]) 1 else -1
-                        posx[j ] += if (posx[j -1] > posx[j ]) 1 else -1
+                        posy[j] += if (posy[j - 1] > posy[j]) 1 else -1
+                        posx[j] += if (posx[j - 1] > posx[j]) 1 else -1
 
-                    } else if (abs(posx[j -1] - posx[j ]) > 1) {
+                    } else if (abs(posx[j - 1] - posx[j]) > 1) {
                         // Sur le meme Y.
-                        posx[j ] += if (posx[j -1] > posx[j ]) 1 else -1
+                        posx[j] += if (posx[j - 1] > posx[j]) 1 else -1
 
-                    } else if (abs((posy[j ] - posy[j -1])) > 1) {
+                    } else if (abs((posy[j] - posy[j - 1])) > 1) {
                         // Sur le meme X
-                        posy[j ] += if (posy[j -1] > posy[j ]) 1 else -1
+                        posy[j] += if (posy[j - 1] > posy[j]) 1 else -1
 
                     }
                 }
 
-                liste.add(position_occupee(posx[9], posy[9]))
+                liste.add(position_occupee(posx[nombreDeNoeuds - 1], posy[nombreDeNoeuds - 1]))
             }
         }
         return liste.size
@@ -96,14 +54,14 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
-    check(part1(testInput) == 13)
-    check(part2(testInput) == 1)
+    check(solution(testInput, 2) == 13)
+    check(solution(testInput, 10) == 1)
     val testInputBis = readInput("Day09_testbis")
-    check(part2(testInputBis) == 36)
+    check(solution(testInputBis, 10) == 36)
 
     val input = readInput("Day09")
-    println(part1(input))
-    println(part2(input))
+    println(solution(input, 2))
+    println(solution(input, 10))
 
 }
 
